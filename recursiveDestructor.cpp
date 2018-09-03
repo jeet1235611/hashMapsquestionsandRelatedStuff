140e2e40bc39fdf7a565f7978dfcf8fd272e04a2
acoustic_jeet <js.jitendra11@gmail.com>
2018-09-03 06:39:23 +0530

    Add files via upload

---
+++ recursiveDestructor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct LNode {
+	int data;
+	LNode* next;
+
+	LNode(int n) {
+		data = n;
+		next = nullptr;
+	}
+
+	void add_to_end(int n) {
+		if(next)
+			next -> add_to_end(n);
+		else
+			next = new LNode(n);
+	}
+	~LNode() {
+		cout << "I am from LNode Destructor " << endl;
+		delete next;
+	}
+};
+
+int main() {
+	LNode root(1);
+	root.add_to_end(2);
+	root.add_to_end(3);
+	root.add_to_end(4);
+}
